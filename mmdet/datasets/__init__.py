dcf1d51bb5ffc352413c952641dde278226ecc89
Etienne David <etidavid1992@gmail.com>
2019-06-06 11:53:11 +0200

    Update __init__.py

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -7,10 +7,10 @@ from .utils import to_tensor, random_scale, show_ann, get_dataset
 from .concat_dataset import ConcatDataset
 from .repeat_dataset import RepeatDataset
 from .extra_aug import ExtraAugmentation
-
+from .capte import CapteDataset
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'GroupSampler',
     'DistributedGroupSampler', 'build_dataloader', 'to_tensor', 'random_scale',
     'show_ann', 'get_dataset', 'ConcatDataset', 'RepeatDataset',
-    'ExtraAugmentation'
+    'ExtraAugmentation','CapteDataset'
 ]
